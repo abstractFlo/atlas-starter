17f80737e535d5c4ad11d623b4633ccac1189d67
Florian Uhlrich <f.uhlrich@abstractmedia.de>
2021-03-07 13:32:05 +0100

    Update starter, add removed files back, add typeorm as devDeps, npm is ignored the peerDependencies

---
+++ resources/gamemode/server/extends/player/alt-player.prototype.ts
@@ -0,0 +1,13 @@
+import { PlayerInterface } from './player.interface';
+import { Player } from 'alt-server';
+import { PlayerExtend } from './player.extend';
+
+declare module 'alt-server' {
+
+
+  export interface Player extends PlayerInterface {
+
+  }
+}
+
+Player.prototype = new PlayerExtend;
